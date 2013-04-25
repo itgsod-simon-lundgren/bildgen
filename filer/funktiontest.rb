9bb5c4b9c2e51b9d0bf7c7e8e9fafcb59a13268a
Simon Lundgren <simon.lundgren@elev.it-gymnasiet.se>
2013-04-25 13:20:22 +0200

    Söker bilder, skapar thumbnail.
    -letar bilder, tar de hittade och gör thumbnail i en annan mapp.

--- filer/funktiontest.rb
+++ filer/funktiontest.rb
@@ -1,12 +1,26 @@
-#Letar i filen "storabilder" efter filer
-class Pictures
+require 'devil'
+
+# class Pictures
+hej = []
 
 files = Dir.glob('storabilder/*') 
 files = files.map { |file| [file.count("/"), file] }
 files = files.sort.map { |file| file[1] }
-files.each do |file|
-  puts file
+files.each do |base|
+hej << File.basename(base)
 end
+
+puts hej
+hej.each do |file|
+
+Dir.chdir('./storabilder')
+puts Dir.getwd
+Devil.with_image(file) do |img|
+    img.thumbnail2(150)
+    img.save("../thumbnails/Thumbnail_#{file}")
+end
+Dir.chdir("..")
+puts Dir.getwd
 end
 
 
