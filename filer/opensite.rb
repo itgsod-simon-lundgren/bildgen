43b47279f7499ba53ee5df1214192283891d8a48
Simon Lundgren <simon.lundgren@elev.it-gymnasiet.se>
2013-05-03 14:42:26 +0200

    Testar att öppna html page etc
    Ska öppna alla bilder i thumbnails mappen.
    inte klar.

--- filer/opensite.rb
+++ filer/opensite.rb
@@ -1,9 +1,25 @@
 
-fileHtml = File.new("galleri.html", "w+")
-fileHtml.close()
-system("start galleri.html")
-
 
 # fileHtml.puts "<HTML><BODY BGCOLOR='grey'>"
 # fileHtml.puts "This is a site with galleries."
 # fileHtml.puts "</BODY></HTML>"
+
+# fileHtml = File.new("galleri.html", "w+")
+# fileHtml.close()
+# system("start galleri.html")
+
+ fileHtml = File.new("galleri.html", "w+")
+ fileHtml.puts "<HTML><BODY BGCOLOR='gray'>"
+
+ # fileHtml.puts "<a href='thumbnails/Thumbnail_*'></a>"
+ fileHtml.puts "<img src='thumbnails/thumbnail_*'>"
+ # fileHtml.puts "<img src='thumbnails/thumbnail_***.jpg'>"
+ fileHtml.puts "<CENTER>Heavy</CENTER><br>"
+ fileHtml.puts "</BODY></HTML>"	
+ fileHtml.close()
+ system("start galleri.html")
+
+
+#  <a href="sunset.gif">
+#   <img src="thmb_sunset.gif" />
+# </a>
